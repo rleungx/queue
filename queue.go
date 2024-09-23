2a00b60ff9f2d944f5cea15d9628f7ea56a776ac
Ryan Leung <rleungx@gmail.com>
2024-09-23 14:26:39 +0800

    implementing more functions
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- queue.go
+++ queue.go
@@ -6,15 +6,16 @@ import (
 	"time"
 )
 
-// PriorityQueue queue has priority and preempt
+// PriorityQueue queue has priority and preempt.
 type PriorityQueue[T comparable] struct {
 	items    map[T]*Entry[T]
 	entries  entryHeap[T]
 	capacity int
-	mu       sync.RWMutex
+	minIndex *int
+	sync.RWMutex
 }
 
-// NewPriorityQueue constructs a priority queue
+// NewPriorityQueue constructs a priority queue.
 func NewPriorityQueue[T comparable](capacity int, cleanupInterval time.Duration) *PriorityQueue[T] {
 	if capacity <= 0 {
 		return nil
@@ -24,56 +25,101 @@ func NewPriorityQueue[T comparable](capacity int, cleanupInterval time.Duration)
 		items:    make(map[T]*Entry[T]),
 		entries:  make(entryHeap[T], 0, capacity),
 		capacity: capacity,
+		minIndex: new(int),
 	}
+	*pq.minIndex = -1
 
-	// Start a goroutine to clean up expired entries periodically
+	// Start a goroutine to clean up expired entries periodically.
 	go func() {
 		ticker := time.NewTicker(cleanupInterval)
 		defer ticker.Stop()
+
 		for range ticker.C {
-			pq.cleanup()
+			pq.Cleanup()
 		}
 	}()
 
 	return pq
 }
 
-func (pq *PriorityQueue[T]) Put(value T, priority int, ttl time.Duration) {
+func (pq *PriorityQueue[T]) Push(value T, priority int, ttl time.Duration) {
 	entry := &Entry[T]{
 		Value:    value,
 		Priority: priority,
 		expireAt: time.Now().Add(ttl),
+		index:    new(int),
 	}
 
-	pq.mu.Lock()
-	defer pq.mu.Unlock()
+	pq.Lock()
+	defer pq.Unlock()
+
+	if _, ok := pq.items[value]; ok {
+		// If the value already exists in the queue, update its priority.
+		pq.items[value].Priority = priority
+		pq.items[value].expireAt = time.Now().Add(ttl)
+
+		return
+	}
+
+	if len(pq.entries) >= pq.capacity {
+		// If the new entry has a lower priority than the lowest priority in the heap, do not add it
+		if pq.entries[*pq.minIndex].Priority > priority {
+			return
+		}
+
+		// Remove the lowest priority element
+		removed, ok := heap.Remove(&pq.entries, *pq.minIndex).(*Entry[T])
+		if !ok {
+			return
+		}
+
+		delete(pq.items, removed.Value)
+	}
 
 	heap.Push(&pq.entries, entry)
 	pq.items[value] = entry
 
-	if len(pq.entries) > pq.capacity {
-		removed := heap.Pop(&pq.entries).(*Entry[T])
-		delete(pq.items, removed.Value)
+	// Update the lowest index.
+	if *pq.minIndex == -1 || pq.entries[*pq.minIndex].Priority >= priority {
+		pq.minIndex = entry.index
 	}
 }
 
-// Tail returns the lowest priority entry
-func (pq *PriorityQueue[T]) Tail() T {
-	pq.mu.RLock()
-	defer pq.mu.RUnlock()
+// Pop returns the highest priority entry.
+func (pq *PriorityQueue[T]) Pop() (t any) {
+	pq.Lock()
+	defer pq.Unlock()
 
 	if len(pq.entries) == 0 {
-		var zero T
-		return zero
+		return t
+	}
+
+	entry, ok := heap.Pop(&pq.entries).(*Entry[T])
+	if !ok {
+		return t
+	}
+
+	delete(pq.items, entry.Value)
+
+	return entry.Value
+}
+
+// Peek returns the highest priority entry.
+func (pq *PriorityQueue[T]) Peek() (t any) {
+	pq.RLock()
+	defer pq.RUnlock()
+
+	if len(pq.entries) == 0 {
+		return t
 	}
 
 	return pq.entries[0].Value
 }
 
-// Elems returns all elements in the queue sorted by priority from high to low
+// Elems returns all elements in the queue sorted by priority from high to low.
 func (pq *PriorityQueue[T]) Elems() []T {
-	pq.mu.RLock()
-	defer pq.mu.RUnlock()
+	pq.RLock()
+	defer pq.RUnlock()
 
 	rs := make([]T, 0, len(pq.entries))
 	tempHeap := make(entryHeap[T], len(pq.entries))
@@ -83,86 +129,119 @@ func (pq *PriorityQueue[T]) Elems() []T {
 			Value:    entry.Value,
 			Priority: entry.Priority,
 			expireAt: entry.expireAt,
-			index:    entry.index,
+			index:    new(int),
 		}
+		*(tempHeap[i].index) = *(entry.index)
 	}
+
 	heap.Init(&tempHeap)
 
 	for tempHeap.Len() > 0 {
-		entry := heap.Pop(&tempHeap).(*Entry[T])
-		if !entry.isExpired() {
+		entry, ok := heap.Pop(&tempHeap).(*Entry[T])
+		if ok && !entry.isExpired() {
 			rs = append(rs, entry.Value)
 		}
-	}
-
-	// Reverse the slice to get elements from high to low priority
-	for i, j := 0, len(rs)-1; i < j; i, j = i+1, j-1 {
-		rs[i], rs[j] = rs[j], rs[i]
 	}
 
 	return rs
 }
 
-// Remove removes a value from the queue
+// Remove removes a value from the queue.
 func (pq *PriorityQueue[T]) Remove(value T) {
-	pq.mu.Lock()
-	defer pq.mu.Unlock()
+	pq.Lock()
+	defer pq.Unlock()
 
 	if entry, ok := pq.items[value]; ok {
 		// Remove the entry from the items map
 		delete(pq.items, value)
 
 		// Remove the entry from the heap
-		if entry.index >= 0 && entry.index < len(pq.entries) {
-			heap.Remove(&pq.entries, entry.index)
+		if *entry.index >= 0 && *entry.index < len(pq.entries) {
+			heap.Remove(&pq.entries, *entry.index)
 		}
 	}
 }
 
-// cleanup removes expired entries from the queue
-func (pq *PriorityQueue[T]) cleanup() {
-	pq.mu.Lock()
-	defer pq.mu.Unlock()
+// Cleanup removes expired entries from the queue.
+func (pq *PriorityQueue[T]) Cleanup() {
+	pq.Lock()
+	defer pq.Unlock()
 
 	now := time.Now()
-	for pq.entries.Len() > 0 {
-		entry := heap.Pop(&pq.entries).(*Entry[T])
+
+	for range pq.entries.Len() {
+		entry, ok := heap.Pop(&pq.entries).(*Entry[T])
+		if !ok {
+			continue
+		}
+
 		if entry.expireAt.After(now) {
 			heap.Push(&pq.entries, entry)
-			break
+
+			continue
 		}
+
 		delete(pq.items, entry.Value)
 	}
 }
 
-// Entry is a pair of region and its priority
+// Capacity returns the capacity of the queue.
+func (pq *PriorityQueue[T]) Capacity() int {
+	pq.RLock()
+	defer pq.RUnlock()
+
+	return pq.capacity
+}
+
+// Size returns the number of elements in the queue.
+func (pq *PriorityQueue[T]) Size() int {
+	pq.RLock()
+	defer pq.RUnlock()
+
+	return len(pq.entries)
+}
+
+// Empty returns true if the queue is empty.
+func (pq *PriorityQueue[T]) Empty() bool {
+	pq.RLock()
+	defer pq.RUnlock()
+
+	return len(pq.entries) == 0
+}
+
+// Entry is a pair of region and its priority.
 type Entry[T any] struct {
 	Priority int
 	Value    T
 	expireAt time.Time
-	index    int // The index of the item in the heap
+	index    *int // The index of the item in the heap.
 }
 
-// isExpired checks if the entry is expired
+// isExpired checks if the entry is expired.
 func (e *Entry[T]) isExpired() bool {
 	return time.Now().After(e.expireAt)
 }
 
-// entryHeap is a min-heap of *Entry[T]
+// entryHeap is a min-heap of *Entry[T].
 type entryHeap[T any] []*Entry[T]
 
 func (h entryHeap[T]) Len() int           { return len(h) }
-func (h entryHeap[T]) Less(i, j int) bool { return h[i].Priority < h[j].Priority }
+func (h entryHeap[T]) Less(i, j int) bool { return h[i].Priority > h[j].Priority }
 func (h entryHeap[T]) Swap(i, j int) {
 	h[i], h[j] = h[j], h[i]
-	h[i].index = i
-	h[j].index = j
+	*h[i].index = i
+	*h[j].index = j
 }
 
 func (h *entryHeap[T]) Push(x any) {
 	n := len(*h)
-	entry := x.(*Entry[T])
-	entry.index = n
+	entry, ok := x.(*Entry[T])
+
+	if !ok {
+		return
+	}
+
+	*entry.index = n
 	*h = append(*h, entry)
 }
 
@@ -170,8 +249,9 @@ func (h *entryHeap[T]) Pop() any {
 	old := *h
 	n := len(old)
 	entry := old[n-1]
-	old[n-1] = nil   // avoid memory leak
-	entry.index = -1 // for safety
+	old[n-1] = nil    // avoid memory leak
+	*entry.index = -1 // for safety
 	*h = old[0 : n-1]
+
 	return entry
 }
--- queue_test.go
+++ queue_test.go
@@ -1,45 +1,51 @@
-package queue
+package queue_test
 
 import (
 	"sync"
 	"testing"
 	"time"
 
+	"github.com/rleungx/queue"
 	"github.com/stretchr/testify/assert"
 )
 
 // TestNewPriorityQueue tests the creation of a new PriorityQueue.
 func TestNewPriorityQueue(t *testing.T) {
-	pq := NewPriorityQueue[int](5, time.Millisecond*100)
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
 	assert.NotNil(t, pq)
-	assert.Equal(t, 5, pq.capacity)
+	assert.Equal(t, 5, pq.Capacity())
 	assert.Empty(t, pq.Elems())
 
 	// Test removing from an empty queue
 	var zero int
+
 	pq.Remove(zero)
 	assert.Empty(t, pq.Elems())
 
 	// Test queue with zero capacity
-	pqZeroCapacity := NewPriorityQueue[int](0, time.Millisecond*100)
+	pqZeroCapacity := queue.NewPriorityQueue[int](0, time.Millisecond*100)
 	assert.Empty(t, pqZeroCapacity)
 
 	// Test queue full capacity
-	pqFull := NewPriorityQueue[int](2, time.Millisecond*100)
-	pqFull.Put(1, 1, time.Millisecond*200)
-	pqFull.Put(2, 2, time.Millisecond*200)
-	pqFull.Put(3, 3, time.Millisecond*200) // This should remove the element with priority 1
+	pqFull := queue.NewPriorityQueue[int](2, time.Millisecond*100)
+	pqFull.Push(1, 1, time.Millisecond*200)
+	pqFull.Push(2, 2, time.Millisecond*200)
+	pqFull.Push(3, 3, time.Millisecond*200) // This should remove the element with priority 1
 	assert.Len(t, pqFull.Elems(), 2)
 	assert.Equal(t, 3, pqFull.Elems()[0])
 	assert.Equal(t, 2, pqFull.Elems()[1])
 }
 
-// TestPutAndElems tests adding elements and retrieving them in the correct order.
-func TestPutAndElems(t *testing.T) {
-	pq := NewPriorityQueue[int](5, time.Millisecond*100)
-	pq.Put(1, 1, time.Second)
-	pq.Put(2, 2, time.Second)
-	pq.Put(3, 3, time.Second)
+// TestPushAndElems tests adding elements and retrieving them in the correct order.
+func TestPushAndElems(t *testing.T) {
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
+	pq.Push(1, 1, time.Second)
+	pq.Push(2, 2, time.Second)
+	pq.Push(3, 3, time.Second)
 	elems := pq.Elems()
 	assert.Len(t, elems, 3)
 	assert.Equal(t, 3, elems[0])
@@ -47,65 +53,141 @@ func TestPutAndElems(t *testing.T) {
 	assert.Equal(t, 1, elems[2])
 }
 
-// TestTail tests the Tail method.
-func TestTail(t *testing.T) {
-	pq := NewPriorityQueue[int](5, time.Millisecond*100)
-	pq.Put(1, 1, time.Second)
-	pq.Put(2, 2, time.Second)
-	pq.Put(3, 3, time.Second)
-	tail := pq.Tail()
-	assert.Equal(t, 1, tail)
+// TestPop tests popping elements from the queue.
+func TestPop(t *testing.T) {
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
+	assert.NotNil(t, pq)
+
+	// Add elements with different priorities
+	pq.Push(1, 10, time.Second)
+	pq.Push(2, 20, time.Second)
+	pq.Push(3, 15, time.Second)
+	pq.Push(4, 5, time.Second)
+	pq.Push(5, 25, time.Second)
+
+	// Pop elements and check the order
+	expectedOrder := []int{5, 2, 3, 1, 4}
+	for _, expected := range expectedOrder {
+		entry := pq.Pop()
+		assert.NotNil(t, entry)
+		assert.Equal(t, expected, entry)
+	}
+
+	// Ensure the queue is empty after popping all elements
+	assert.Nil(t, pq.Pop())
+	assert.Empty(t, pq.Elems())
+}
+
+// TestPeek tests peeking the highest priority element.
+func TestPeek(t *testing.T) {
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
+	pq.Push(1, 1, time.Second)
+	pq.Push(3, 2, time.Second)
+	pq.Push(2, 3, time.Second)
+	peek := pq.Peek()
+	assert.Equal(t, 2, peek)
 }
 
 // TestRemove tests removing an entry from the queue.
 func TestRemove(t *testing.T) {
-	pq := NewPriorityQueue[int](5, time.Millisecond*100)
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
 
 	// Add an entry to the queue.
-	pq.Put(1, 1, time.Second) // This adds an entry with value 1 and priority 1.
+	pq.Push(1, 1, time.Second) // This adds an entry with value 1 and priority 1.
 
-	// Retrieve the entry to remove it.
-	entryToRemove := pq.entries[0] // Get the entry we just added.
-
-	// Now remove the actual entry from the queue.
-	pq.Remove(entryToRemove.Value)
+	// Now remove the entry from the queue.
+	pq.Remove(1)
 
 	// Check the elements after removal.
 	assert.Empty(t, pq.Elems())
 }
 
+// TestSizeAndEmpty tests getting the size of the queue and checking if it is empty.
+func TestSizeAndEmpty(t *testing.T) {
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
+	assert.NotNil(t, pq)
+
+	// Initially, the size should be 0
+	assert.Equal(t, 0, pq.Size())
+
+	// Add elements to the queue
+	pq.Push(1, 10, time.Second*1)
+	pq.Push(2, 20, time.Second*1)
+	pq.Push(3, 15, time.Second*1)
+
+	// The size should be 3
+	assert.Equal(t, 3, pq.Size())
+
+	// Add more elements to the queue
+	pq.Push(4, 5, time.Second*1)
+	pq.Push(5, 25, time.Second*1)
+
+	// The size should be 5
+	assert.Equal(t, 5, pq.Size())
+
+	// Pop elements from the queue
+	pq.Pop()
+	pq.Pop()
+
+	// The size should be 3
+	assert.Equal(t, 3, pq.Size())
+
+	// Pop all elements from the queue
+	pq.Pop()
+	pq.Pop()
+	pq.Pop()
+
+	// The queue should be empty again
+	assert.True(t, pq.Empty())
+}
+
 // TestExpiration tests the expiration of entries.
 func TestExpiration(t *testing.T) {
-	pq := NewPriorityQueue[int](5, time.Millisecond*100)
-	pq.Put(1, 1, time.Millisecond*50)
-	pq.Put(2, 2, time.Millisecond*50)
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*100)
+	pq.Push(1, 1, time.Millisecond*50)
+	pq.Push(2, 2, time.Millisecond*50)
 	time.Sleep(time.Millisecond * 60) // Wait for entries to expire.
-	pq.cleanup()                      // Manually trigger cleanup.
+	pq.Cleanup()                      // Manually trigger cleanup.
 	assert.Empty(t, pq.Elems())
 }
 
 // TestCleanup tests the automatic cleanup of expired entries.
 func TestCleanup(t *testing.T) {
-	pq := NewPriorityQueue[int](5, time.Millisecond*50)
-	pq.Put(1, 1, time.Millisecond*50)
-	pq.Put(2, 2, time.Millisecond*50)
+	t.Parallel()
+	pq := queue.NewPriorityQueue[int](5, time.Millisecond*50)
+	pq.Push(1, 1, time.Millisecond*50)
+	pq.Push(2, 1, time.Millisecond*50)
+	pq.Push(3, 1, time.Millisecond*100)
+	pq.Push(4, 1, time.Millisecond*100)
+	pq.Push(5, 1, time.Millisecond*100)
 	time.Sleep(time.Millisecond * 60) // Wait for entries to expire.
+	assert.Len(t, pq.Elems(), 3)
 
-	// Let the cleanup goroutine run.
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * 60) // Wait for entries to expire.
 	assert.Empty(t, pq.Elems())
 }
 
 // TestPriorityAndExpiration tests that higher priority entries are not removed when they are still valid.
 func TestPriorityAndExpiration(t *testing.T) {
-	pq := NewPriorityQueue[int](10, time.Millisecond*100)
-	pq.Put(1, 1, time.Millisecond*100) // Low priority
-	pq.Put(2, 3, time.Millisecond*50)  // High priority
-	pq.Put(3, 2, time.Millisecond*100) // Medium priority
+	t.Parallel()
+	pq := queue.NewPriorityQueue[int](10, time.Millisecond*100)
+	pq.Push(1, 1, time.Millisecond*100) // Low priority
+	pq.Push(2, 3, time.Millisecond*50)  // High priority
+	pq.Push(3, 2, time.Millisecond*100) // Medium priority
 
 	time.Sleep(time.Millisecond * 60) // Wait for the high priority entry to expire.
 	// Trigger cleanup
-	pq.cleanup()
+	pq.Cleanup()
 
 	elems := pq.Elems()
 	assert.Len(t, elems, 2)
@@ -121,18 +203,21 @@ func TestPriorityAndExpiration(t *testing.T) {
 		assert.True(t, found)
 		delete(expectedValues, elem) // Remove found element
 	}
+
 	assert.Empty(t, expectedValues)
 }
 
-// TestCleanupWithActiveEntries tests that cleanup does not remove active entries.// TestCleanupWithActiveEntries tests that cleanup does not remove active entries.
+// TestCleanupWithActiveEntries tests that cleanup does not remove active entries.
 func TestCleanupWithActiveEntries(t *testing.T) {
-	pq := NewPriorityQueue[int](10, time.Millisecond*100)
+	t.Parallel()
 
-	pq.Put(1, 1, time.Millisecond*50)  // Low priority, should expire quickly
-	pq.Put(2, 2, time.Millisecond*200) // High priority, should still be active
+	pq := queue.NewPriorityQueue[int](10, time.Millisecond*100)
+
+	pq.Push(1, 1, time.Millisecond*50)  // Low priority, should expire quickly
+	pq.Push(2, 2, time.Millisecond*200) // High priority, should still be active
 
 	time.Sleep(time.Millisecond * 70) // Wait for the low priority entry to expire.
-	pq.cleanup()                      // Manually trigger cleanup.
+	pq.Cleanup()                      // Manually trigger cleanup.
 
 	elems := pq.Elems()
 	assert.Len(t, elems, 1)
@@ -141,28 +226,37 @@ func TestCleanupWithActiveEntries(t *testing.T) {
 
 // TestConcurrentAddAndRemove tests adding and removing entries concurrently.
 func TestConcurrentAddAndRemove(t *testing.T) {
-	pq := NewPriorityQueue[int](10, time.Millisecond*100)
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](10, time.Millisecond*100)
 	for i := 1; i <= 5; i++ {
-		pq.Put(i, i, time.Second) // Each entry has a TTL of 1s
+		pq.Push(i, i, time.Second) // Each entry has a TTL of 1s
 	}
 
 	var wg sync.WaitGroup
+
+	funcP := func(val int) {
+		defer wg.Done()
+		pq.Push(val, val, time.Second) // Each entry has a TTL of 1s
+	}
+
+	funcR := func(val int) {
+		defer wg.Done()
+		pq.Remove(val)
+	}
+
 	// Adding entries concurrently
 	for i := 6; i <= 10; i++ {
 		wg.Add(1)
-		go func(val int) {
-			defer wg.Done()
-			pq.Put(val, val, time.Second) // Each entry has a TTL of 1s
-		}(i)
+
+		go funcP(i)
 	}
 
 	// Removing entries concurrently
 	for i := 1; i <= 5; i++ {
 		wg.Add(1)
-		go func(val int) {
-			defer wg.Done()
-			pq.Remove(val)
-		}(i)
+
+		go funcR(i)
 	}
 
 	wg.Wait() // Wait for all goroutines to finish
@@ -173,48 +267,59 @@ func TestConcurrentAddAndRemove(t *testing.T) {
 
 // TestPriorityChange tests changing the priority of an entry.
 func TestPriorityChange(t *testing.T) {
-	pq := NewPriorityQueue[int](10, time.Millisecond*100)
-	pq.Put(1, 1, time.Second) // Low priority
-	pq.Put(2, 3, time.Second) // High priority
-	pq.Put(3, 2, time.Second) // Medium priority
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](10, time.Millisecond*100)
+	pq.Push(1, 1, time.Second) // Low priority
+	pq.Push(2, 3, time.Second) // High priority
+	pq.Push(3, 2, time.Second) // Medium priority
 
 	// Change priority of entry with value 1 to a higher priority
-	entryToChange := pq.entries[0] // This should be the entry with value 1
-	pq.Remove(entryToChange.Value) // Remove it first
-	pq.Put(1, 4, time.Second)      // Add it back with a higher priority
+	pq.Remove(1)               // Remove it first
+	pq.Push(1, 4, time.Second) // Add it back with a higher priority
 	elems := pq.Elems()
 	assert.Len(t, elems, 3)
 	// Check the order: highest priority (4) should come first
 	assert.Equal(t, 1, elems[0])
+
+	// Change priority of entry with value 3 to a lower priority
+	pq.Push(2, 1, time.Second) // Change the priority
+	elems = pq.Elems()
+	assert.Len(t, elems, 3)
+	// Check the order: lowest priority (1) should come last
+	assert.Equal(t, 2, elems[2])
 }
 
 // TestOrderAfterMultipleRemovals tests the order of elements after multiple removals.
 func TestOrderAfterMultipleRemovals(t *testing.T) {
-	pq := NewPriorityQueue[int](10, time.Millisecond*100)
-	pq.Put(4, 1, time.Second)      // Low priority
-	pq.Put(3, 3, time.Second)      // High priority
-	pq.Put(2, 2, time.Second)      // Medium priority
-	pq.Put(1, 4, time.Second)      // Highest priority
-	pq.Remove(pq.entries[0].Value) // Remove the lowest priority (1)
-	pq.Remove(pq.entries[1].Value) // Remove the next (2)
+	t.Parallel()
+
+	pq := queue.NewPriorityQueue[int](10, time.Millisecond*100)
+	pq.Push(4, 1, time.Second) // Low priority
+	pq.Push(3, 3, time.Second) // High priority
+	pq.Push(2, 2, time.Second) // Medium priority
+	pq.Push(1, 4, time.Second) // Highest priority
+	pq.Remove(1)               // Remove the highest priority
+	pq.Remove(3)               // Remove the next
 
 	elems := pq.Elems()
 	assert.Len(t, elems, 2)
-	assert.Equal(t, 1, elems[0])
-	assert.Equal(t, 2, elems[1])
+	assert.Equal(t, 2, elems[0])
+	assert.Equal(t, 4, elems[1])
 }
 
 // TestMixedOperations tests mixed operations of adding, removing, and expiring entries.
 func TestMixedOperations(t *testing.T) {
-	pq := NewPriorityQueue[int](10, time.Millisecond*100)
+	t.Parallel()
+	pq := queue.NewPriorityQueue[int](10, time.Millisecond*100)
 
 	// Add entries
-	pq.Put(1, 1, time.Millisecond*50)  // Low priority, should expire quickly
-	pq.Put(2, 3, time.Millisecond*200) // High priority
-	pq.Put(3, 2, time.Millisecond*100) // Medium priority
+	pq.Push(1, 1, time.Millisecond*50)  // Low priority, should expire quickly
+	pq.Push(2, 3, time.Millisecond*200) // High priority
+	pq.Push(3, 2, time.Millisecond*100) // Medium priority
 
 	time.Sleep(time.Millisecond * 80) // Wait for the low priority entry to expire
-	pq.cleanup()                      // Clean up expired entries
+	pq.Cleanup()                      // Clean up expired entries
 
 	// Check remaining entries
 	elems := pq.Elems()
@@ -231,16 +336,16 @@ func TestMixedOperations(t *testing.T) {
 	assert.Equal(t, 3, elems[0])
 }
 
-func TestPriorityQueueWithDifferentTypes(t *testing.T) {
+func TestDifferentTypes(t *testing.T) {
+	t.Parallel()
+
 	// Test with int type
-	pqInt := NewPriorityQueue[int](10, time.Millisecond*100)
-	pqInt.Put(1, 1, time.Millisecond*50)
-	pqInt.Put(2, 3, time.Millisecond*200)
-	pqInt.Put(3, 2, time.Millisecond*100)
-
+	pqInt := queue.NewPriorityQueue[int](10, time.Millisecond*100)
+	pqInt.Push(1, 1, time.Millisecond*50)
+	pqInt.Push(2, 3, time.Millisecond*200)
+	pqInt.Push(3, 2, time.Millisecond*100)
 	time.Sleep(time.Millisecond * 60)
-	pqInt.cleanup()
-
+	pqInt.Cleanup()
 	elemsInt := pqInt.Elems()
 	assert.Len(t, elemsInt, 2)
 	assert.Equal(t, 2, elemsInt[0])
@@ -250,14 +355,12 @@ func TestPriorityQueueWithDifferentTypes(t *testing.T) {
 	assert.Equal(t, 3, elemsInt[0])
 
 	// Test with string type
-	pqString := NewPriorityQueue[string](10, time.Millisecond*100)
-	pqString.Put("a", 1, time.Millisecond*50)
-	pqString.Put("b", 3, time.Millisecond*200)
-	pqString.Put("c", 2, time.Millisecond*100)
-
+	pqString := queue.NewPriorityQueue[string](10, time.Millisecond*100)
+	pqString.Push("a", 1, time.Millisecond*50)
+	pqString.Push("b", 3, time.Millisecond*200)
+	pqString.Push("c", 2, time.Millisecond*100)
 	time.Sleep(time.Millisecond * 60)
-	pqString.cleanup()
-
+	pqString.Cleanup()
 	elemsString := pqString.Elems()
 	assert.Len(t, elemsString, 2)
 	assert.Equal(t, "b", elemsString[0])
@@ -272,19 +375,16 @@ func TestPriorityQueueWithDifferentTypes(t *testing.T) {
 		Name string
 	}
 
-	pqStruct := NewPriorityQueue[CustomStruct](10, time.Millisecond*100)
-	pqStruct.Put(CustomStruct{ID: 1, Name: "a"}, 1, time.Millisecond*50)
-	pqStruct.Put(CustomStruct{ID: 2, Name: "b"}, 3, time.Millisecond*200)
-	pqStruct.Put(CustomStruct{ID: 3, Name: "c"}, 2, time.Millisecond*100)
-
+	pqStruct := queue.NewPriorityQueue[CustomStruct](10, time.Millisecond*100)
+	pqStruct.Push(CustomStruct{ID: 1, Name: "a"}, 1, time.Millisecond*50)
+	pqStruct.Push(CustomStruct{ID: 2, Name: "b"}, 3, time.Millisecond*200)
+	pqStruct.Push(CustomStruct{ID: 3, Name: "c"}, 2, time.Millisecond*100)
 	time.Sleep(time.Millisecond * 60)
-	pqStruct.cleanup()
-
+	pqStruct.Cleanup()
 	elemsStruct := pqStruct.Elems()
 	assert.Len(t, elemsStruct, 2)
 	assert.Equal(t, 2, elemsStruct[0].ID)
 	assert.Equal(t, "b", elemsStruct[0].Name)
-
 	pqStruct.Remove(elemsStruct[0])
 	elemsStruct = pqStruct.Elems()
 	assert.Len(t, elemsStruct, 1)
